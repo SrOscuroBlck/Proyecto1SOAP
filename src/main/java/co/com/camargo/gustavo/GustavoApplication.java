83dad82caf37b7757fd4a14fcbd868f0ffcc4ed5
Gustavo Camargo <camargogustavoa@gmail.com>
2024-08-13 20:04:02 -0500

    Finished

---
+++ src/main/java/co/com/camargo/gustavo/GustavoApplication.java
@@ -0,0 +1,13 @@
+package co.com.camargo.gustavo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GustavoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GustavoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/com/camargo/gustavo/GustavoApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.camargo.gustavo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GustavoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
